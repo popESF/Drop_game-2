826f9b312b740ecab70183435955780eaf0e1605
HERNK <89403578+popESF@users.noreply.github.com>
2022-10-30 17:19:44 +0000

    Added game files

---
+++ game2/core/src/com/game2/game/MainMenuScreen.java
@@ -0,0 +1,62 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainMenuScreen implements Screen {
+
+    final Drop game;
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final Drop gam) {
+        game = gam;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 0);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "DMC:6 ", 100, 150);
+        game.font.draw(game.batch, "Click anywhere to start collecting your MOTIVATION!", 100, 100);
+        game.font.draw(game.batch, "Can you collect 20?", 100, 50);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
